a26c86d673e4183b7aa810f73142e4e6c44b428d
Yaroslav337 <pelerovteatr@gmail.com>
2023-06-28 23:15:14 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import throttle from 'lodash.throttle';
+import photoCardTemplate from './templates/photo-card.hbs';
 
 const refs = {
   html: document.documentElement,
@@ -38,43 +39,7 @@ const checkCollectionLimit = ({ data }) => {
 };
 
 const renderMarkup = ({ data: { hits: photos } }) => {
-  const markup = photos
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `
-        <div class="photo-card">
-        <a href="${largeImageURL}">
-      <img src="${webformatURL}" width="300" height="300" alt="${tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p class="info-item">
-        Likes
-          <b>${likes}</b>
-        </p>
-        <p class="info-item">
-        Views
-          <b>${views}</b>
-        </p>
-        <p class="info-item">
-        Comments
-          <b>${comments}</b>
-        </p>
-        <p class="info-item">
-        Downloads
-          <b>${downloads}</b>
-        </p>
-      </div>
-    </div>`
-    )
-    .join('');
+  const markup = photoCardTemplate(photos);
 
   refs.galleryEl.insertAdjacentHTML('beforeend', markup);
 };
